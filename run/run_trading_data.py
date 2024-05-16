bd6f7474257e412c9bc64821dfa29eb0deda4613
Vu Tan Huynh <vuh@Vu-Hs-MacBook-Air.local>
2024-05-16 14:30:20 -0400

    udpate testing

--- run/run_trading_data.py
+++ run/run_trading_data.py
@@ -3,6 +3,7 @@ import logging
 from datetime import datetime, timedelta
 from argparse import ArgumentParser
 import os
+import pandas as pd
 
 # Set up logging
 logging.basicConfig(filename='trading_log.log', level=logging.INFO, 
@@ -21,20 +22,22 @@ def run_script(script_name, contract, time, interval):
         logging.error(f"Script {script_name} failed with error: {e}")
 
 if __name__ == '__main__':
-
     # testing
-    param = {
-        "contract": "0xcBC67Ea382F8a006d46EEEb7255876BeB7d7f14d", "time": "weekly", "interval": "12"
-    }
+    sample_df = pd.read_csv('sample_data.csv')
+    # print(sample_df['contract_address'])
+    for i in sample_df['contract_address'][:10]:
+        param = {
+            "contract": i, "time": "weekly", "interval": "12"
+        }
 
-    repo_root = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
-    scripts_directory = "get_trading_data"
-    scripts_directory = os.path.join(repo_root, scripts_directory)
+        repo_root = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir))
+        scripts_directory = "get_trading_data"
+        scripts_directory = os.path.join(repo_root, scripts_directory)
 
 
-    for filename in os.listdir(scripts_directory):
-        if filename.endswith(".py") and filename.startswith("get_"):
-            # Construct the full file path
-            script_path = os.path.join(scripts_directory, filename)
-            print(script_path)
-            run_script(script_path, param['contract'], param['time'], param['interval'])
+        for filename in os.listdir(scripts_directory):
+            if filename.endswith(".py") and filename.startswith("get_"):
+                # Construct the full file path
+                script_path = os.path.join(scripts_directory, filename)
+                print(script_path)
+                run_script(script_path, param['contract'], param['time'], param['interval'])
